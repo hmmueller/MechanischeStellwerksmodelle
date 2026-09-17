dc858da569437fe42cb3f3e22642e3804dd3046e
Harald M. <harald_m_mueller@gmx.de>
2026-09-17 12:33:14 +0200

    Helper-Datei umbenannt, Pfadregeln angepasst
    Änderungen
    Keine.
    
    Organisatorische Anpassungen
    - Sonderzeichen.txt --> CADHelpers.txt
    - Pfadregeln angepasst:
      PQ -> A: ??; PQ -> PQ: Q wird für Vervielfachungen von P verwendet.
      LM -> LM: M wird für Vervielfachungen von L verwendet; LM -> PQ: Beim Aufspannen werden L/M- und P/Q-Pfade oft gemeinsam aufgespannt.
      RS -> RS: S wird für Vervielfachungen von R verwendet
      T  -> T: T-Pfade werden für T-Fräser verwendet und können daher mit nichts sonst gemischt werden.

--- PathDxf2GCode.bat
+++ PathDxf2GCode.bat
@@ -1,2 +1,8 @@
 @echo off
-CALL \Private\CNCFraese\PathDxf2GCode -eP$:[APQ]$ -eL$:[LP]$ -eR$:R$ -eT$:T$ -eQ$:[PQ$] %1 %2 %3 %4 %5 %6 %7 %8 %9
+REM E-Regeln:
+REM  PQ -> A: ??; PQ -> PQ: Q wird für Vervielfachungen von P verwendet.
+REM  LM -> LM: M wird für Vervielfachungen von L verwendet; LM -> PQ: Beim Aufspannen werden L/M- und P/Q-Pfade oft gemeinsam aufgespannt.
+REM  RS -> RS: S wird für Vervielfachungen von R verwendet
+REM  T  -> T: T-Pfade werden für T-Fräser verwendet und können daher mit nichts sonst gemischt werden.
+
+CALL \Private\CNCFraese\PathDxf2GCode -e[PQ]$:[APQ]$ -e[LM]$:[LMPQ]$ -e[RS]$:[RS]$ -eT$:T$ %1 %2 %3 %4 %5 %6 %7 %8 %9
